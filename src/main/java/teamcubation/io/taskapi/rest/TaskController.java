9415721512a2a02b6bc58f488eeb0e642ab9aa44
jack <ext_japompeu@mercadolivre.com>
2024-01-18 11:41:45 -0300

    fixing DispatcherServlet configuration handler error.

--- src/main/java/teamcubation/io/taskapi/rest/TaskController.java
+++ src/main/java/teamcubation/io/taskapi/rest/TaskController.java
@@ -5,10 +5,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:8081")
-@RestController("/api/tasks")
+@RestController
+@RequestMapping("/api/tasks")
 public class TaskController {
 
 
